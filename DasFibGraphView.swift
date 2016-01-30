a20d0ff65963784a86d051ef2066f27390347ad4
Guthrie Kuckes <guthriekuckes@gmail.com>
2016-01-30 13:32:53 -0500

    better drawing

--- DasFibGraphView.swift
+++ DasFibGraphView.swift
@@ -35,7 +35,25 @@ private let graphLineWidth : CGFloat = 1
 private let PHILineWidth : CGFloat = 3.0;
 
 
-private let BabyPowder = NSColor(colorLiteralRed: 254, green: 254, blue: 250, alpha: 1.0)
+//this is not being used. note it is specified incorrectly
+///private let BabyPowder = NSColor(colorLiteralRed: 254, green: 254, blue: 250, alpha: 1.0)
+
+
+//the grey color of the lines on the graph.
+private let ourGrey = NSColor(colorLiteralRed: 172/255, green: 170/255, blue: 171/255, alpha: 1.0  )
+
+
+
+extension NSBezierPath {
+    
+    func lineFrom(point: CGPoint, toPoint: CGPoint) {
+        
+        self.moveToPoint(point)
+        self.lineToPoint(toPoint)
+    }
+}
+
+
 
 
 public class fibGraphView : NSView  {
@@ -48,6 +66,26 @@ public class fibGraphView : NSView  {
     //the scale of the graph. in px : 1 unit
     private (set) var coordinateDilation : CGFloat = 100
     
+    //the queue operations not happening on the main queue happen on while drawing this view
+    private let graphMaker = NSOperationQueue()
+
+    
+    
+    //have our own initalizer, to be able to better initalize graphMaker
+    override init(frame frameRect: NSRect) {
+        
+        super.init(frame: frameRect)
+        
+        self.graphMaker.maxConcurrentOperationCount = 2
+    }
+    
+
+    //required if we want to have our own initializer.
+    required public init?(coder: NSCoder) {
+        
+        super.init(coder: coder)
+        //fatalError("init(coder:) has not been implemented")
+    }
     
     
     //A / B starts refers to a mathematics paper on fibonacci numbers I read. 
@@ -110,66 +148,92 @@ public class fibGraphView : NSView  {
         
         
         var horizontalBezier = NSBezierPath()
+        var horizontalBold = NSBezierPath()
+        
         var verticalBezier = NSBezierPath()
-
+        var verticalBold = NSBezierPath()
+        
         
         //for concurrency, the horizontal graph lines and vertical graph line making will be divided into seperate blocks to be executed concurrently on the same queue
         func makeHorizontalBezierPath() -> Void {
             
             let numberOfTimesHorizontal : CGFloat = CGFloat( floor(startingY / coordinateDilation) ) //I think this is to get something on the exact line, in case the cg interval is not so ezact
             
-        
+            
+            var timesThroughLoop = 0
+            
             for var Index : CGFloat = numberOfTimesHorizontal * coordinateDilation; Index < endingY ; Index += coordinateDilation { //cg interval is thus the number of pixels which equals a scale of one.
                 
                 let newStartPoint = CGPointMake(startingX, Index)
                 let newEndPoint = CGPointMake(endingX, Index)
                 
                 
+                if(timesThroughLoop % 5 == 0) {
+                    
+                    horizontalBold.lineFrom(newStartPoint, toPoint: newEndPoint)
+                    
+                } else {
+                    
+                    horizontalBezier.lineFrom(newStartPoint, toPoint: newEndPoint)
+                }
+                
                 horizontalStart.append(newStartPoint) //add them to a new array too make the labeling easier
                 horizontalEnd.append(newEndPoint)
                 
-                horizontalBezier.moveToPoint(newStartPoint)
-                horizontalBezier.lineToPoint(newEndPoint)
+                timesThroughLoop++
             }
             
             horizontalBezier.lineWidth = graphLineWidth
+            horizontalBold.lineWidth = graphLineWidth + 1
         }
         
         
         func makeVerticalBezierPath() -> Void {
             
             let numberOfTimesVertical : CGFloat = floor(startingY / coordinateDilation)
+            
+            var timesThroughLoop = 0
+            
             for var Index : CGFloat = numberOfTimesVertical * coordinateDilation ; Index < endingX ; Index += coordinateDilation {
                 
                 let newStartPoint = CGPointMake(Index, startingY)
                 let newEndPoint = CGPointMake(Index, endingY)
                 
                 
+                if(timesThroughLoop % 5 == 0) {
+                    
+                    verticalBold.lineFrom(newStartPoint, toPoint: newEndPoint)
+                } else {
+                    
+                    verticalBezier.lineFrom(newStartPoint, toPoint: newEndPoint)
+                }
+                
                 verticalStart.append(newStartPoint)
                 verticalEnd.append(newEndPoint)
                 
-                verticalBezier.moveToPoint(newStartPoint)
-                verticalBezier.lineToPoint(newEndPoint)
+                timesThroughLoop++
             }
-
+            
             
             verticalBezier.lineWidth = graphLineWidth
+            verticalBold.lineWidth = graphLineWidth + 1
         }
         
 
         
-        let graphMaker = NSOperationQueue()
-        graphMaker.addOperationWithBlock(makeHorizontalBezierPath)
-        graphMaker.addOperationWithBlock(makeVerticalBezierPath)
-        graphMaker.waitUntilAllOperationsAreFinished()
-        
-        
-        
         if(coordinateDilation > 0.9) { //this creates the effect of having zommed out of everything
 
-            NSColor.blackColor().setStroke()
+            graphMaker.addOperationWithBlock(makeHorizontalBezierPath)
+            graphMaker.addOperationWithBlock(makeVerticalBezierPath)
+            graphMaker.waitUntilAllOperationsAreFinished()
+            
+            ourGrey.setStroke()
             horizontalBezier.stroke()
-            verticalBezier.stroke() //note assuming color here
+            verticalBezier.stroke()
+            
+            NSColor.blackColor().setStroke()
+            horizontalBold.stroke()
+            verticalBold.stroke()
         }
         
 
--- FibiPHI/AppDelegate.swift
+++ FibiPHI/AppDelegate.swift
@@ -13,28 +13,34 @@ import AppKit
 var DasSharedDelegate : AppDelegate? = nil
 
 
-private let aboutOptions = ["Copyright" : "Copyright 2015 Guthrie Kuckes" , "Version" : "1.0.0" , "Credits" : NSAttributedString(string: "All code in is in Apple's swift language and was written by Guthrie Kuckes in February 2015 for a school project") ]
-
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate  {
 
+    
+    
     @IBOutlet weak var window: NSWindow!
 
+    //shows us wether we are using PHI or its reciprocal
     @IBOutlet weak var phiValueText: NSTextField!
     
+    //the menu item to click to switch between PHI and its repciprocal
     @IBOutlet weak var PHIreciprocalMenuItem: NSMenuItem!
     
+    //the custom view graphing the fibonnacci sequence
     @IBOutlet weak var fibGraphingView: fibGraphView!
 
+    //where you enter the starting number b of the sequence
     @IBOutlet weak var startingNumberField: NSTextField!
     
+    //one of the ways to change the graph scale
     @IBOutlet weak var ScaleSlider: NSSlider!
     
-    
+    //shows with numbers how we are getting closer to PHI
     @IBOutlet weak var resiudalTableView: NSTableView!
     
     
+    //what happens when you loick on the menu item to switch between phi and its reciprocal
     @IBAction func wantsPHIreciprocal(sender: AnyObject) {
         
         print("reciprocation desired")
@@ -44,6 +50,7 @@ class AppDelegate: NSObject, NSApplicationDelegate  {
         self.fibGraphingView.setNeedsDisplayInRect(fibGraphingView.bounds)
         self.resiudalTableView.reloadData()
     }
+    
     
     @IBAction func takeScaleSliderIntValue(sender: AnyObject) {
         
@@ -62,6 +69,7 @@ class AppDelegate: NSObject, NSApplicationDelegate  {
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        
         DasSharedDelegate = self
         self.ScaleSlider.floatValue = 100.0
         self.resiudalTableView.setDataSource(self.fibGraphingView.fibonnaciNumbers)
@@ -75,7 +83,10 @@ class AppDelegate: NSObject, NSApplicationDelegate  {
     
     @IBOutlet weak var aboutMenuItem: NSMenuItem!
     
+    
     @IBAction func aboutDocAction(sender: AnyObject) {
+        
+                let aboutOptions = ["Copyright" : "Copyright 2016 Guthrie Kuckes" , "Version" : "1.0.0" , "Credits" : NSAttributedString(string: "All code in is in Apple's swift language, and the program was originally written in 2015 for a school project") ]
                 NSApplication.sharedApplication().orderFrontStandardAboutPanelWithOptions(aboutOptions)
     }
     
